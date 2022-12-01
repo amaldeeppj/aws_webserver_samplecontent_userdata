606561ac82fbb0c3ba92098e262e8d5cf9a5bf09
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-12-01 19:18:54 +0530

    chown

--- userdata.sh
+++ userdata.sh
@@ -10,5 +10,5 @@ amazon-linux-extras install php7.4  -y
 systemctl enable httpd.service
 git clone https://github.com/amaldeeppj/webserver_sample_content.git /var/website/
 cp -r  /var/website/*  /var/www/html/
-chown -R apache:apache /var/www/html/*
+chown -R apache:apache /var/www/html/
 systemctl restart httpd.service
